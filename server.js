41fb544be1056959f23291c8b9aac94d1e208546
athomas227 <andrewthomas@pursuit.org>
2024-03-02 08:09:38 -0500

    finished exercise

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.post('/submit-data', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
 
 module.exports = app;
